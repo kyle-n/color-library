6f14946d5e9966179e05abbbd854c2ea2cdcbcda
Kyle Nazario <kylebnazario@gmail.com>
2024-09-01 12:55:12 -0600

    Color number input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'color-library';
+  protected readonly minVal = 0;
+  protected readonly maxVal = 255;
+  protected title = 'color-library';
+  protected colorNumber = new FormControl<number>(0, {
+    nonNullable: true,
+    validators: [Validators.min(this.minVal), Validators.max(this.maxVal)],
+  });
+  protected savedColorNumber: number | undefined;
+
+  protected saveColorNumber(): void {
+    const colorNumberWithinLimits = Math.min(this.maxVal, Math.max(this.minVal, this.colorNumber.value));
+    this.colorNumber.setValue(colorNumberWithinLimits);
+    this.savedColorNumber = colorNumberWithinLimits;
+  }
 }
